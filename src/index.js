13e1d5f3f7cf14ea30b8385e8c1ce7393d305e5a
Fran Raga <franka1986@gmail.com>
2020-03-12 20:39:14 +0100

    start (error)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { render } from "react-dom";
+import {
+    render
+} from "react-dom";
 import OLMapFragment from "./components/OLMapSample";
 import "./index.css";
+// OSM Buildings
+import OSMBuildings from './OSMBuildings-OL5.js';
 
-render(<OLMapFragment />, document.getElementById("root"));
+render( < OLMapFragment / > , document.getElementById("root"));
