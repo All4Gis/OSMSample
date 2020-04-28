51e7426ed85920857114a942da28d00926f1c4c7
Fran Raga <franka1986@gmail.com>
2020-04-28 19:31:35 +0200

    Updates

---
+++ precache-manifest.44926999144e8c6c56800b491bd78dee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87aa24d565321e7f0b12e27104ad2af",
+    "url": "/OSMSample/index.html"
+  },
+  {
+    "revision": "690698a2806f20744271",
+    "url": "/OSMSample/static/css/2.db529fa1.chunk.css"
+  },
+  {
+    "revision": "63a955ad5e7090369d90",
+    "url": "/OSMSample/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "690698a2806f20744271",
+    "url": "/OSMSample/static/js/2.35a4bb0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/OSMSample/static/js/2.35a4bb0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a955ad5e7090369d90",
+    "url": "/OSMSample/static/js/main.dcb9f93e.chunk.js"
+  },
+  {
+    "revision": "7e83d4a0ead078cdfe0e",
+    "url": "/OSMSample/static/js/runtime-main.8e92b853.js"
+  }
+]);
